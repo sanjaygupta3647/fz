29a54e6f0506199273c19084de8a7882900ebfd1
akanksha237 <akanksha11103416@gmail.com>
2015-02-16 00:23:12 +0530

    aa

--- common_css.php
+++ common_css.php
@@ -3,4 +3,4 @@
 <link href="<?=SITE_PATH?>css/banner_section.css" rel="stylesheet" type="text/css" />
 <link href="<?=SITE_PATH?>css/latest_updates.css" rel="stylesheet" type="text/css" />
 <link href="<?=SITE_PATH?>css/demo_popup.css" rel="stylesheet" type="text/css" media="screen">
-<link href="<?=SITE_PATH?>colorbox/colorbox.css" rel="stylesheet" type="text/css" />
+<link href="<?=SITE_PATH?>colorbox/colorbox.css" rel="stylesheet" type="text/css" />   
